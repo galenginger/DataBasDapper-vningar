cc0fec4c95cbd03083cac5712c2c187804f51a99
Sebastian <93seblin@skola.boras.se>
2025-10-28 13:27:56 +0100

    Lägg till alla ändringar

---
+++ 251028/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+using Dapper;
+using System.Data.Common;
+class Program
+{
+    static void Main()
+    {
+        //skapa en connetionstring mot min databas
+        string constring = File.ReadLines("Connectionstring.txt").First();
+
+
+        //using IDbConnection connection = new MySqlConnection(
+
+
+        //skapa ett IDbconnetion-objekt (dbcon) av MySQL-typ:
+        using IDbConnection dbcon = new MySqlConnection(constring);
+
+        //lägg in lärare:
+        dbcon.Execute("INSERT INTO Teacher(Name, Email) VALUES ('Janne Långben', 'janne@suvnet.se');");
+
+        //skapa en lista med teachers, fykk på den med data från ett result set
+        //genom att anropa dapper-metoden Query()
+        IEnumerable<Teacher> teachers = dbcon.Query<Teacher>("select Name, Email from Teacher;");
+
+        //loopa igenom teachers-ienumerable (typ lista) och skriv ut :
+        foreach (Teacher t in teachers)
+        {
+            Console.WriteLine($"Hello from {t.Name}, you can reach me at {t.Email};");
+        }
+
+    }
+}
---
+++ 251028/Teacher.cs
@@ -0,0 +1,9 @@
+
+//string connectionString = "Server=min.adress.skola.com;Database=schooldb;User Id=root;Password=yourpassword;";
+//using IDbConnection db = new MySqlConnection(connectionString);
+
+class Teacher
+{
+    public string Name { set; get; }
+    public string Email { set; get; }
+}
